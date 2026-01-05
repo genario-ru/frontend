cb0e5c2dfcbed939c07c1e9ccd9e565819a60bd0
ilialksv <iliaalekseevofb@gmail.com>
2026-01-06 01:11:40 +0800

    fix: fixes radio group

--- src/shared/components/ui/radio-cards-group.tsx
+++ src/shared/components/ui/radio-cards-group.tsx
@@ -8,8 +8,8 @@ const radioCardsGroupItemVariants = cva(
   cn(
     "flex w-full flex-col items-center font-medium ring ring-neutral-4 bg-neutral-1 duration-200",
     "[&_svg]:stroke-neutral-7 [&_svg]:shrink-0 [&_svg]:size-6 ",
-    "hover:bg-neutral-2 active:bg-neutral-2 focus:bg-neutral-2",
-    "data-[state=checked]:bg-neutral-2 data-[state=checked]:ring-neutral-8 data-[state=checked]:ring-2 data-[state=checked]:[&_svg]:stroke-neutral-8",
+    "hover:bg-neutral-2 active:bg-neutral-2",
+    "data-[state=checked]:ring-neutral-8 data-[state=checked]:ring-2 data-[state=checked]:[&_svg]:stroke-neutral-8",
   ),
   {
     variants: {
