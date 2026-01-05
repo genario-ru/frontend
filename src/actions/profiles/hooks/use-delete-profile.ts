58b1c35f1c0c63d82f19472cf9c536b70ef55834
ilialksv <iliaalekseevofb@gmail.com>
2026-01-06 01:05:57 +0800

    fix: fixes query invalidation on profiles page

--- src/actions/profiles/hooks/use-delete-profile.ts
+++ src/actions/profiles/hooks/use-delete-profile.ts
@@ -1,6 +1,9 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import { deleteApiV1ProfilesProfileIdMutation } from "@/codegen/api/product/@tanstack/react-query.gen";
+import {
+  deleteApiV1ProfilesProfileIdMutation,
+  getApiV1ProfilesMyQueryKey,
+} from "@/codegen/api/product/@tanstack/react-query.gen";
 import { useToast } from "@/shared/hooks/use-toast";
 
 type UseDeleteProfileParams = {
@@ -8,6 +11,7 @@ type UseDeleteProfileParams = {
 };
 
 export function useDeleteProfile(params?: UseDeleteProfileParams) {
+  const queryClient = useQueryClient();
   const { onSuccess } = params ?? {};
   const { showErrorToast, showSuccessToast } = useToast();
 
@@ -21,10 +25,15 @@ export function useDeleteProfile(params?: UseDeleteProfileParams) {
         });
       },
       onSuccess: ({ data }) => {
+        queryClient.invalidateQueries({
+          queryKey: getApiV1ProfilesMyQueryKey(),
+        });
+
         showSuccessToast({
           title: "Профиль удален",
           description: `Профиль "${data.name}" был успешно удален`,
         });
+
         onSuccess?.();
       },
     });
