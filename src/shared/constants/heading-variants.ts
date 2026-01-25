2327badfaf17cbe3a44b1987012abac50fbfe3e4
ilialksv <iliaalekseevofb@gmail.com>
2026-01-26 01:28:15 +0800

    fix: fixed headings styles

--- src/shared/constants/heading-variants.ts
+++ src/shared/constants/heading-variants.ts
@@ -3,10 +3,10 @@ import { cva } from "class-variance-authority";
 export const headingVariants = cva("w-fit", {
   variants: {
     variant: {
-      h1: "text-2xl font-bold",
-      h2: "text-xl font-bold",
+      h1: "text-2xl font-semibold",
+      h2: "text-xl font-semibold",
       h3: "text-lg font-semibold",
-      h4: "text-base font-semibold",
+      h4: "text-base font-medium",
       h5: "text-sm font-medium",
       h6: "text-xs font-medium",
     },
