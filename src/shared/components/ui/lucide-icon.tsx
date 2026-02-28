0c7f795334194a488dd6d9870160e98b62ddc86c
ilialksv <iliaalekseevofb@gmail.com>
2026-03-01 04:41:15 +0500

    fix: implement custom dynamic lucide icon in order not to make 1500 requests for god damn icons

--- src/shared/components/ui/lucide-icon.tsx
+++ src/shared/components/ui/lucide-icon.tsx
@@ -1,5 +1,7 @@
 import type { LucideIcon as LucideIconType, LucideProps } from "lucide-react";
-import { DynamicIcon, type IconName, iconNames } from "lucide-react/dynamic";
+import type { ComponentType, LazyExoticComponent } from "react";
+import { lazy, Suspense } from "react";
+import { pascalCase } from "text-case";
 
 import { svgIconVariants } from "@/shared/constants/svg-icon-variants";
 import { cn } from "@/shared/utils/cn";
@@ -11,6 +13,32 @@ type Icon = LucideIconType | string;
 export type LucideIconProps = LucideProps &
   SVGIconVariantProps & { icon: Icon };
 
+type LucideComponent = ComponentType<LucideProps>;
+
+const iconCache = new Map<string, LazyExoticComponent<LucideComponent>>();
+
+const getLazyIcon = (iconName: string) => {
+  const normalizedIconName = pascalCase(iconName);
+  const cachedIcon = iconCache.get(normalizedIconName);
+
+  if (cachedIcon) {
+    return cachedIcon;
+  }
+
+  const LazyIcon = lazy(() =>
+    import("lucide-react").then((module) => {
+      const IconComponent =
+        module.icons[normalizedIconName as keyof typeof module.icons];
+
+      return { default: IconComponent };
+    }),
+  );
+
+  iconCache.set(normalizedIconName, LazyIcon);
+
+  return LazyIcon;
+};
+
 export const LucideIcon = ({
   icon: Icon,
   size,
@@ -20,20 +48,16 @@ export const LucideIcon = ({
   ...props
 }: LucideIconProps) => {
   if (typeof Icon === "string") {
-    if (iconNames.includes(Icon as IconName)) {
-      return (
-        <DynamicIcon
-          // @ts-expect-error TODO: Попробовать разобраться, почему тут TypeScript выделывается
-          name={Icon as IconName}
+    const LazyIcon = getLazyIcon(Icon);
+
+    return (
+      <Suspense fallback={null}>
+        <LazyIcon
           className={cn(svgIconVariants({ size, color, priority }), className)}
           {...props}
         />
-      );
-    }
-
-    console.warn(`Icon ${Icon} is not Lucide icon`);
-
-    return null;
+      </Suspense>
+    );
   }
 
   return (
