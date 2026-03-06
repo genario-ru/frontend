a52df3543ba29034e53f363a295346d5e325df32
ilialksv <iliaalekseevofb@gmail.com>
2026-03-06 14:32:35 +0500

    fix: added retries only for suitable error codes

--- src/lib/tanstack-query/create-query-client.ts
+++ src/lib/tanstack-query/create-query-client.ts
@@ -1,7 +1,10 @@
 import { QueryClient } from "@tanstack/react-query";
 import { millisecondsInMinute, secondsInMinute } from "date-fns/constants";
 
+import { APIError } from "../api/classes/api-error";
+
 const MAX_RETRY_COUNT = 3;
+const RETRY_STATUS_CODES = [408, 429, 500, 502, 503, 504];
 
 export function createQueryClient() {
   const queryClient = new QueryClient({
@@ -9,7 +12,16 @@ export function createQueryClient() {
       queries: {
         staleTime: 5 * secondsInMinute * millisecondsInMinute,
         refetchOnReconnect: true,
-        retry: (failureCount) => failureCount < MAX_RETRY_COUNT,
+        retry: (failureCount, error) => {
+          if (error instanceof APIError) {
+            return (
+              RETRY_STATUS_CODES.includes(error.cause.status) &&
+              failureCount < MAX_RETRY_COUNT
+            );
+          }
+
+          return failureCount < MAX_RETRY_COUNT;
+        },
       },
     },
   });
