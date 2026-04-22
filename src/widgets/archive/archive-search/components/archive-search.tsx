89f3da2fc3fe5ec52a85c318d4c5b1fb2837711b
ilialksv <iliaalekseevofb@gmail.com>
2026-04-22 05:10:53 +0500

    fix: removed unnecessary classes

--- src/widgets/archive/archive-search/components/archive-search.tsx
+++ src/widgets/archive/archive-search/components/archive-search.tsx
@@ -14,14 +14,12 @@ export function ArchiveSearch() {
   } = useArchiveSearch();
 
   return (
-    <form onSubmit={onFormSubmit} className="flex min-w-0 flex-1 gap-2">
-      <div className="min-w-0 flex-1">
-        <form.AppField name="q">
-          {(field) => (
-            <field.InputField placeholder="Заголовок или описание..." />
-          )}
-        </form.AppField>
-      </div>
+    <form onSubmit={onFormSubmit} className="flex flex-1 gap-2">
+      <form.AppField name="q">
+        {(field) => (
+          <field.InputField placeholder="Заголовок или описание..." />
+        )}
+      </form.AppField>
       {isDraftDirty && (
         <form.AppForm>
           <form.SubmitButton priority="secondary" icon={<SearchIcon />} />
