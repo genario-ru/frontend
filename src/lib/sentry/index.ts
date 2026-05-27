e80a5f655ca85383e774a87d5f48828ca8bb111d
ilialksv <iliaalekseevofb@gmail.com>
2026-05-28 04:40:16 +0500

    feat: removed unnecessary files

--- src/lib/sentry/index.ts
+++ src/lib/sentry/index.ts
@@ -1,3 +1,2 @@
 export { initSentry } from "./client/init-sentry";
-export { createSentryRouterOnCatch } from "./client/sentry-router";
 export { clearSentryUser, setSentryUser } from "./client/sentry-user";
