9ab202c2f2cf1adb1e3c0971e02de741995bad9e
ilialksv <iliaalekseevofb@gmail.com>
2026-06-15 04:03:05 +0500

    fix: fixed badge

--- src/features/landing/landing-hero/components/landing-hero-badge.tsx
+++ src/features/landing/landing-hero/components/landing-hero-badge.tsx
@@ -9,7 +9,7 @@ export function LandingHeroBadge() {
       color="custom"
       className="bg-accent-1 text-accent-7 [&_svg]:stroke-accent-7"
     >
-      Для YouTube, RuTube, VK Видео, Dzen и других платформ
+      Для коротких и длинных видео
     </Badge>
   );
 }
